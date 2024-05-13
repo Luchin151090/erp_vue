4d47f9c6300ab53e5d9271d46aa7bca4063c4746
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-05-13 13:36:36 -0500

    ss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+//VUETIFY
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// COMPONENTS
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+createApp(App).use(vuetify).mount('#app')
